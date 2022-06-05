deb762a58ad4038903bf96308b0a4177f4508872
VanesStalkers <ivan.mironov@bk.ru>
2022-06-05 19:20:25 +0300

    * переписал ядро на классы (с class build-дерами)

--- index.js
+++ index.js
@@ -3,7 +3,6 @@
 import config from "./config.js";
 import pg from "pg";
 
-import telegramBot from "./src/TelegramBot.class.js";
 import Lobby from "./src/Lobby.class.js";
 
 console.error = (...arg) =>
@@ -13,114 +12,17 @@ console.error = (...arg) =>
       .concat(["\x1b[30m"])
       .join(" ")
   );
-``;
-export class errorCatcher {
-  constructor(err) {
+globalThis.errorCatcher = class {
+  constructor(err, knownError) {
     console.error(err?.message);
-    console.log(err);
+    if (!knownError) console.log(err);
   }
-}
+};
 
 try {
-  process.CONFIG = config;
-  process.DB = new pg.Pool(config.postgres);
-  process.BOT = new telegramBot();
-  await process.BOT.setCommands([
-    { command: "/start", description: "Стартовое сообщение" },
-  ]);
-
-  process.LST = {};
-  process.CONSTANTS = {};
-  process.BOT_MENU = {};
-  process.BOT_COMMANDS = {
-    "/start": async function (argList) {
-      await this.startMsg(argList);
-    },
-  };
-
-  process.LOBBY = new Lobby();
-
-  process.BOT.setHandler({
-    handler: "message",
-    action: async function (msg) {
-      try {
-        //console.log("message", msg);
-
-        const userId = msg.from.id;
-        const chatId = msg.chat.id;
-        const msgId = msg.message_id;
-
-        // пока что функционал обработки событий от ботов не нужен
-        // + после pinChatMessage прилетает сообщение от бота, которое ломает getUser
-        if (msg.from.is_bot) return;
-
-        const user = await process.LOBBY.getUser({ userId, chatId });
-        if (!user) return;
-
-        const text = msg.text;
-
-        if (user.currentAction?.onTextReceivedHandler) {
-          user.currentAction?.onTextReceivedHandler({ text });
-          return;
-        }
-
-        if (!msg.entities) {
-          const menuItem = process.BOT_MENU[text];
-          if (menuItem) {
-            if (typeof menuItem.action === "function") {
-              await menuItem.action.call({ user, msg });
-            }
-          } else {
-          }
-        } else {
-          for (let i = 0; i < msg.entities.length; i++) {
-            const entity = msg.entities[i];
-            const command = text.substring(
-              entity.offset,
-              entity.offset + entity.length
-            );
-            if (process.BOT_COMMANDS[command]) {
-              await process.BOT_COMMANDS[command].call(user, { msgId, text });
-            }
-          }
-        }
-      } catch (err) {
-        new errorCatcher(err);
-      }
-    },
-  });
-
-  process.BOT.setHandler({
-    handler: "callback_query",
-    action: async (msg) => {
-      try {
-        //console.log("callback_query", msg);
-
-        const userId = msg.from.id;
-        const chatId = msg.message.chat.id;
-        const msgId = msg.message.message_id;
-
-        // пока что функционал обработки событий от ботов не нужен
-        if (msg.from.is_bot) return;
-
-        const user = await process.LOBBY.getUser({ userId, chatId });
-
-        const action = msg.data.split("__");
-        const actionFunc = user[action[0]];
-
-        if (actionFunc) {
-          await actionFunc.call(user, { msgId, data: action });
-        } else {
-          user.sendSystemErrorMsg({
-            err: new Error(`action "${action[0]}" not found`),
-          });
-          //throw new Error(`action "${action[0]}" not found`);
-        }
-      } catch (err) {
-        new errorCatcher(err);
-      }
-    },
-  });
+  globalThis.CONFIG = config;
+  globalThis.DB = new pg.Pool(CONFIG.postgres);
+  globalThis.LOBBY = await Lobby.build();
 } catch (err) {
   switch (err.message) {
     default:
@@ -128,3 +30,7 @@ try {
       break;
   }
 }
+
+process.on("unhandledRejection", (reason, promise) => {
+  console.log("Unhandled Rejection at:", promise, "reason:", reason);
+});
--- src/User.class.js
+++ src/User.class.js
@@ -1,11 +1,17 @@
-import { simpleMsgWrapper } from "./BotQueryHelper.js";
+import { BuildableClass } from "./Base.class.js";
+
 import { simpleMsgWrapper, checkListMsgWrapper } from "./BotQueryHelper.js";
 
-export default class User {
+export default class User extends BuildableClass {
   id;
-  constructor() {}
-  async init({ userId, userData = {}, telegramData = {} } = {}) {
-    const queryData = await process.DB.query(
+  #textHandlerList = {};
+  #menuReady = false;
+  constructor(userData = {}) {
+    super(...arguments);
+    this.id = userData.id;
+  }
+  static async build({ userId, userData = {}, telegramData = {} } = {}) {
+    const queryData = await DB.query(
       `
                 SELECT u.id, u.data
                 FROM users u
@@ -16,7 +22,7 @@ export default class User {
     );
     const user = queryData.rows[0] || {};
     if (!user.id) {
-      const queryResult = await process.DB.query(
+      const queryResult = await DB.query(
         `
                     INSERT INTO users (id, data, telegram, last_login)
                     VALUES ($1, $2, $3, NOW()::timestamp)
@@ -24,11 +30,10 @@ export default class User {
                 `,
         [userId, userData, telegramData]
       );
-      this.id = queryResult.rows[0].id;
+      user.id = queryResult.rows[0].id;
       user.data = userData;
     } else {
-      this.id = user.id;
-      await process.DB.query(
+      await DB.query(
         `
                     UPDATE users
                     SET telegram = $1, last_login = NOW()::timestamp
@@ -37,18 +42,27 @@ export default class User {
         [telegramData, userId]
       );
     }
-    return this;
+    return new User({ ...user, createdFromBuilder: true });
   }
-  async startMsg({ msg } = {}) {
-    await process.BOT.sendMessage(
-      simpleMsgWrapper.call(this, {
-        text: `Привет, друг!`,
-        inlineKeyboard: [[{ text: "И тебе привет", callback_data: "hello" }]],
-      })
-    );
+
+  async lastMsgCheck({ msgId } = {}) {
+    const activeEvent = !msgId && this.lastMsg?.id;
+    const oldEvent =  msgId && this.lastMsg?.id !== msgId;
+    if (activeEvent || oldEvent) {
+      await this.sendSimpleError({
+        error:
+          this.lastMsg?.lastMsgCheckErrorText ||
+          (oldEvent
+            ? "Эта задача уже не актуальна"
+            : "Последнее действие должно быть завершено"),
+      });
+      return false;
+    } else {
+      return true;
+    }
   }
   resetCurrentAction() {
-    delete this.lastMsgId;
+    delete this.lastMsg;
     delete this.currentAction;
     delete this.checkList;
   }
@@ -56,7 +70,7 @@ export default class User {
     const sorryText = `У нас тут что-то сломалось, но программисты уже все чинят. Попробуй обновить меня командой /start и попробовать все заново.\n`;
     const errText = `\nError message: '${err?.message}'.`;
 
-    await process.BOT.sendMessage(
+    await BOT.sendMessage(
       simpleMsgWrapper.call(this, {
         text: sorryText + errText,
         entities: [
@@ -64,5 +78,85 @@ export default class User {
         ],
       })
     );
+  }
+
+  startMenuMarkup() {
+    return [
+    ];
+  }
+  menuItem(item) {
+    if (item.actionHandler)
+      this.#textHandlerList[item.text] = item.actionHandler;
+    return { ...item, actionHandler: undefined };
+  }
+  menuReady(value) {
+    if (value !== undefined) {
+      this.#menuReady = value;
+    } else {
+      return this.#menuReady;
+    }
+  }
+  getMenuHandler(handler) {
+    return this.#textHandlerList[handler];
+  }
+  async handleMenuAction(handler) {
+    if (!this.menuReady()) {
+      this.startMenuMarkup.call(this);
+      this.menuReady(true);
+    }
+    const menuHandler = this.getMenuHandler(handler);
+    if (menuHandler) {
+      return await menuHandler.call(this);
+    } else {
+      return false;
+    }
+  }
+
+  async startMsg({ msg } = {}) {
+    await BOT.sendMessage(
+      simpleMsgWrapper.call(this, {
+        text: `Приветствую! У вас есть меню.`,
+        keyboard: this.startMenuMarkup.call(this),
+      })
+    );
+    this.menuReady(true);
+    await BOT.sendMessage(
+      simpleMsgWrapper.call(this, {
+        text: `У вас есть вопросы?`,
+        inlineKeyboard: [
+          [{ text: "Вопросов нет, давай начинать!", callback_data: "hello" }],
+        ],
+      })
+    );
+  }
+
+  async help() {
+    if (!this.currentAction) {
+      await this.sendSimpleAnswer({
+        text: "Выберите одно из действий, которые можно совершить.",
+      });
+    } else {
+      this.currentAction?.help();
+    }
+  }
+  async sendSimpleAnswer({ text }) {
+    const inlineKeyboard = [];
+    await BOT.sendMessage({
+      chatId: this.currentChat,
+      text,
+      inlineKeyboard,
+    });
+  }
+  async sendSimpleError({ error }) {
+    await BOT.sendMessage({
+      chatId: this.currentChat,
+      text:
+        "<b>Ошибка</b>: " +
+        error +
+        (this.lastMsgId
+          ? "\n<i>Актуальная задача прикреплена к данному сообщению.</i>"
+          : ""),
+      replyId: this.lastMsgId,
+    });
   }
 }
